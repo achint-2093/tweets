c49da2e41e2006c2186d7bc2062d8f3692c4b29a
achint2093 <=achintwadhwa418@gmail.com>
2024-05-07 19:27:15 +0530

    added image picker

--- app/src/main/java/com/techuntried/jetpackcomposemvvm/screens/composeui/ImagePicker.kt
+++ app/src/main/java/com/techuntried/jetpackcomposemvvm/screens/composeui/ImagePicker.kt
@@ -1,5 +1,7 @@
 package com.techuntried.jetpackcomposemvvm.screens.composeui
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
@@ -11,31 +13,54 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.techuntried.jetpackcomposemvvm.R
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun ImagePickerScreen() {
+    var selectedImageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = {
+            selectedImageUri = it
+        }
+    )
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            modifier = Modifier
-                .padding(0.dp, 8.dp)
-                .clip(RoundedCornerShape(12.dp)),
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = ""
-        )
-        Button(onClick = {}) {
+        if (selectedImageUri != null) {
+            val paint = rememberImagePainter(data = selectedImageUri, builder = {
+                placeholder(R.drawable.ic_launcher_background)
+                crossfade(1000)
+            })
+            Image(
+                modifier = Modifier
+                    .padding(0.dp, 8.dp)
+                    .clip(RoundedCornerShape(12.dp)),
+                painter = paint,
+                contentDescription = ""
+            )
+        }
+        Button(onClick = {
+            singlePhotoPickerLauncher.launch(
+                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
+        }) {
             Text(text = "Pick Image")
         }
     }
